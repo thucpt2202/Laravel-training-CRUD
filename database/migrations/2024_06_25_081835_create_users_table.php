69aa27a1b05ba0d309a73e2255719f4228ec8e3b
Pham Tri Thuc <thucpt@runsystem.net>
2024-06-25 16:56:32 +0700

    feat: create login, signup, reset password page

--- database/migrations/2024_06_25_081835_create_users_table.php
+++ database/migrations/2024_06_25_081835_create_users_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
-            $table->timestamp('expires_at')->nullable();
-            $table->timestamps();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->enum('role', ['nuser', 'admin', 'oper']);
+            $table->enum('status', ['active', 'inactive']);
+            $table->softDeletes(); // deleted_at
+            $table->timestamps();  // created_at và updated_at
         });
     }
 
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('personal_access_tokens');
+        Schema::dropIfExists('users');
     }
 };
